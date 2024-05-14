d0f1c8bd79dac3673e20a6f0070634ba63c53f6e
lendikriz13 <165079807+lendikriz13@users.noreply.github.com>
2024-05-13 23:58:50 -0500

    Added Campsite Details/Directory

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import CampsitesList from './features/campsites/CampsitesList';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <><Header /><div className="App">
-      <CampsitesList />
+      <CampsitesDirectoryPage />
       <Footer />
     </div></>
   );
