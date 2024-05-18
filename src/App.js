568c3af144e3aecc7f72d54b3a29ad7736ba5419
lendikriz13 <165079807+lendikriz13@users.noreply.github.com>
2024-05-18 13:52:06 -0500

    About Page Content

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
               path='directory/:campsiteId'
               element={<CampsiteDetailPage />}
         />
+        <Route path='about' element={<AboutPage />} />
       </Routes>
       <Footer />
     </div></>
